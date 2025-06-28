8d0c0c4397149a025f21e9b78afa10417affc2bd
Nitesh Kumar singh <116357832+niixxks@users.noreply.github.com>
2025-06-28 22:46:14 +0530

    Time: 1 ms (87.19%), Space: 41.2 MB (21.07%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,5 +1,17 @@
 class Solution {
     public int mySqrt(int x) {
-       return (int)Math.sqrt(x);
+      if (x==0) return 0;
+      int l=1, r=x, ans=0; 
+       while(l<=r){
+         int mid=(l+r)/2;
+         if(mid<=x/mid){
+            ans= mid;
+            l=mid+1;
+         }
+         else{
+            r=mid-1;
+         }
+       }
+       return ans;
     }
 }

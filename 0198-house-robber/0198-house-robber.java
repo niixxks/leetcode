6debfef6b7ec50aca47aee8a01c10b66d5f8ded5
Nitesh Kumar singh <116357832+niixxks@users.noreply.github.com>
2025-07-10 22:51:01 +0530

    Time: 0 ms (100%), Space: 41 MB (76.53%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,15 +1,17 @@
 class Solution {
-    public int solve(int i , int[] nums ,int[] dp){
-        if (i==0) return nums[i];
-        if(i<0) return 0;
-         if(dp[i] != -1) return dp[i];
-        int rob = nums[i] + solve(i-2 , nums ,dp);
-        int notrob = solve(i-1, nums , dp);
-        return dp[i]= Math.max(rob , notrob);
+    public int houserob(int n , int[] nums ,int[] dp){
+        dp[0]= nums[0];
+        for(int i = 1 ; i<nums.length; i++){
+         int rob = nums[i];
+         if(i>1) rob = nums[i] + dp[i-2];
+         int notrob = dp[i-1];
+         dp[i]= Math.max(notrob , rob);
+        }
+        return dp[nums.length - 1];
     }
     public int rob(int[] nums) {
         int [] dp = new int[nums.length];
-        Arrays.fill(dp,-1);
-      return solve(nums.length-1, nums ,dp);  
+        Arrays.fill(dp,0);
+      return houserob(nums.length-1, nums ,dp);  
     } 
 }
